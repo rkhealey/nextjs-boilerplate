fff8e0585080e5d19fd4eade03251ae1e952e3c5
rkhealey <hello@rkhealey.com>
2018-05-16 22:11:43 +1000

    add test script

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
-}
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+  ],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/.next/',
+    '<rootDir>/src/pages/_app.js',
+    '<rootDir>/src/redux/store.js',
+    '<rootDir>/src/redux/reducers/index.js',
+  ],
+  snapshotSerializers: [
+    '<rootDir>/node_modules/enzyme-to-json/serializer',
+  ],
+};
